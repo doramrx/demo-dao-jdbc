746f8a76f00b4939bcd6087c6fe22ad9b1757a63
Isadora <isah3027@gmail.com>
2023-10-29 14:29:20 -0300

    findAll implementation

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -21,5 +21,11 @@ public class Main {
         for(Seller s : list){
             System.out.println(s);
         }
+
+        System.out.println("\n==== Test 3: seller findAll ====");
+        list = sellerDao.findAll();
+        for(Seller s : list){
+            System.out.println(s);
+        }
     }
 }
